2d8d8d72a80f3ad0df608ecc33280f968223cede
leeyh <leeyh@isung.com>
2019-11-14 20:54:05 +0900

    init

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ST_Unitas_iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ST_Unitas_iOS
+  
+  pod 'Kingfisher', '~> 5.9'
+  pod 'Alamofire', '5.0.0-rc.3'
+
+end
---
+++ ST_Unitas_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ST_Unitas_iOS
+//
+//  Created by lee on 2019/11/14.
+//  Copyright © 2019 lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
